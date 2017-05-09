1d36a65ca35f84754e49ec9b5bbdf671c9618fe6
ColinRosati1 <colin.james.rosati@gmail.com>
2017-05-09 12:32:38 -0400

    ump send and receive working to be tested across machines

--- UDP_SERVER/Rutgers-UDP-Tut/UDP-Tutorial04.c
+++ UDP_SERVER/Rutgers-UDP-Tut/UDP-Tutorial04.c
@@ -9,48 +9,93 @@
 
 #define PORT 5555
 
-int main(int argc, char *argv[]){
+// int main(int argc, char *argv[]){
 
-	int i;
-	int fd; // socet name
-	struct hostent *hp; // host info
-	struct in_addr **addr_list;
-	char host;                                         ///bug is the host
-	struct sockaddr_in servaddr; // serve address
+// 	int i;
+// 	int fd; // socket name
+// 	struct hostent *hp; // host info
+// 	struct in_addr **addr_list;
+// 	char *host;                                         ///bug is the host
+// 	struct sockaddr_in servaddr; // server address
+// 	char *my_message = "this is a test message";
+
+// 	/* fill in the server's address and data */
+// 	memset((char*)&servaddr, 0, sizeof(servaddr));
+// 	servaddr.sin_family = AF_INET;
+// 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+// 	servaddr.sin_port = htons(PORT);
+
+// 	inet_pton(AF_INET, "127.0.0.1", &addr_list);
+// 	hp = gethostbyaddr(&addr_list, sizeof addr_list, AF_INET);
+
+// 	 if (argc != 2) {
+//         fprintf(stderr,"usage: ghbn hostname\n");
+//         return 1;
+//     }
+
+//     if ((hp = gethostbyname(argv[1])) == NULL) {  // get the host info
+//         herror("gethostbyname");
+//         return 2;
+//     }
+
+//     printf("Official name is: %s\n", hp->h_name);
+//     printf("    IP addresses: ");
+//     addr_list = (struct in_addr **)hp->h_addr_list;
+//     for(i = 0; addr_list[i] != NULL; i++) {
+//         printf("%s ", inet_ntoa(*addr_list[i]));
+//     }
+//     printf("\n");
+
+// 	/* look up the address of the server given its name */
+// 	 hp = gethostbyname(host);                                  ///bug is the host
+// 	 if (!hp) {
+// 	 	fprintf(stderr, "could not obtain address of %s\n", host);
+// 	 	return 0;
+// 	 }
+
+// 	/* put the host's address into the server address structure */
+// 	//memcpy((void *)&servaddr.sin_addr, hp->h_addr_list[0], hp->h_length);
+
+// 	/* send a message to the server */
+// 	if (sendto(fd, my_message, strlen(my_message), 0, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+// 		perror("sendto failed");
+// 		return 0;
+// 	}
+
+// }
+
+int main(void){
+
+	int fd;
+	struct hostent *hp;     /* host information */
+	struct sockaddr_in servaddr, remaddr;    /* server address */
 	char *my_message = "this is a test message";
+	char *server = "127.0.0.1";	
+
+	if ( (fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { //AF_NET is the Address, SOCK_DGRAM is what type of data we are streaming
+        perror("socket failed");
+        return 1;
+    }
+	
 
 	/* fill in the server's address and data */
 	memset((char*)&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(PORT);
 
-	inet_pton(AF_INET, "127.0.0.1", &addr_list);
-	host = gethostbyaddr(&addr_list, sizeof addr_list, AF_INET);
-
-	 if (argc != 2) {
-        fprintf(stderr,"usage: ghbn hostname\n");
-        return 1;
-    }
-
-    if ((hp = gethostbyname(argv[1])) == NULL) {  // get the host info
-        herror("gethostbyname");
-        return 2;
-    }
-
-     printf("Official name is: %s\n", hp->h_name);
-    printf("    IP addresses: ");
-    addr_list = (struct in_addr **)hp->h_addr_list;
-    for(i = 0; addr_list[i] != NULL; i++) {
-        printf("%s ", inet_ntoa(*addr_list[i]));
-    }
-    printf("\n");
+	if (bind(fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+		perror("bind failed");
+		return 0;
+	}  
+	
 
 	/* look up the address of the server given its name */
-	 hp = gethostbyname(host);                                  ///bug is the host
-	 if (!hp) {
-	 	fprintf(stderr, "could not obtain address of %s\n", host);
-	 	return 0;
-	 }
+	hp = gethostbyname(server);
+	if (!hp) {
+		fprintf(stderr, "could not obtain address of %s\n", server);
+		return 0;
+	}
 
 	/* put the host's address into the server address structure */
 	//memcpy((void *)&servaddr.sin_addr, hp->h_addr_list[0], hp->h_length);
@@ -59,6 +104,5 @@ int main(int argc, char *argv[]){
 	if (sendto(fd, my_message, strlen(my_message), 0, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
 		perror("sendto failed");
 		return 0;
-	}
-
+}
 }
---
+++ UDP_SERVER/Rutgers-UDP-Tut/udp-send.c
@@ -0,0 +1,93 @@
+/*
+        demo-udp-03: udp-send: a simple udp client
+	send udp messages
+	This sends a sequence of messages (the # of messages is defined in MSGS)
+	The messages are sent to a port defined in SERVICE_PORT 
+
+        usage:  udp-send
+
+        Paul Krzyzanowski
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h> // Needed to use struct sockaddr_in
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
+
+#define PORT 21234
+
+#define BUFLEN 2048
+#define MSGS 5	/* number of messages to send */
+
+
+int main(void)
+{
+	struct sockaddr_in myaddr, remaddr;
+	int fd, i, slen=sizeof(remaddr);
+	char buf[BUFLEN];	/* message buffer */
+	int recvlen;		/* # bytes in acknowledgement message */
+	char *server = "127.0.0.1";	/* change this to use a different server */
+	char *my_message = "this is a test message";
+
+	/* create a socket */
+
+	if ((fd=socket(AF_INET, SOCK_DGRAM, 0))==-1)
+		printf("socket created\n");
+
+	/* bind it to all local addresses and pick any port number */
+
+	memset((char *)&myaddr, 0, sizeof(myaddr));
+	myaddr.sin_family = AF_INET;
+	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	myaddr.sin_port = htons(0);
+
+	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+		perror("bind failed");
+		return 0;
+	}       
+
+	/* now define remaddr, the address to whom we want to send messages */
+	/* For convenience, the host address is expressed as a numeric IP address */
+	/* that we will convert to a binary format via inet_aton */
+
+	memset((char *) &remaddr, 0, sizeof(remaddr));
+	remaddr.sin_family = AF_INET;
+	remaddr.sin_port = htons(PORT);
+	if (inet_aton(server, &remaddr.sin_addr)==0) {
+		fprintf(stderr, "inet_aton() failed\n");
+		//exit(1);
+	}
+
+	/* now let's send the messages */
+
+	for (i=0; i < MSGS; i++) {
+		printf("Sending packet %d to %s port %d\n", i, server, PORT);
+		sprintf(buf, "This is packet %d", i);
+		if (sendto(fd, my_message, strlen(my_message), 0, (struct sockaddr *)&remaddr, slen)==-1) {
+			perror("sendto");
+			return 0;
+			//exit(1);
+		}
+		/* now receive an acknowledgement from the server */
+		recvlen = recvfrom(fd, buf, BUFLEN, 0, (struct sockaddr *)&remaddr, &slen);
+                if (recvlen >= 0) {
+                        buf[recvlen] = 0;	/* expect a printable string - terminate it */
+                        printf("received message: \"%s\"\n", buf);
+                        return 0;
+                }
+	}
+
+	// if (sendto(fd, my_message, strlen(my_message), 0, (struct sockaddr *)&remaddr, sizeof(remaddr)) < 0) {
+	// 	perror("sendto failed");
+	// 	return 0;
+	// }
+
+	//close(fd);
+	//return 0;
+}
